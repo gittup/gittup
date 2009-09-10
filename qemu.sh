7bbaffb17f70edc9d17f3c49e6ced3e9b0d5c420
marf <marfey@gmail.com>
2009-09-09 21:09:44 -0400

    Kernel boots and plays nethack.

--- qemu.sh
+++ qemu.sh
@@ -1,3 +1,2 @@
 echo "[33mDon't forget to press ctrl-alt-3[0m"
-qemu -hda /home/marf/foo.img -kernel /home/marf/gittup/linux/arch/x86/boot/bzImage -append console=ttyS0
-#qemu linux.img -hdb fat:/my_directory
+qemu -hda /home/marf/foo.img -kernel /home/marf/gittup/linux/arch/x86/boot/bzImage -append "console=ttyS0 root=/dev/sda rw init=/sbin/init"
