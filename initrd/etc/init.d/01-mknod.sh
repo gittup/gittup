a68d96c88ed11e9305370b5c148ec8036a386e62
marf <marfey@gmail.com>
2009-11-19 23:28:53 -0500

    Added some new /dev entries, permissions.

--- initrd/etc/init.d/01-mknod.sh
+++ initrd/etc/init.d/01-mknod.sh
@@ -1,3 +1,16 @@
+mknod /dev/null c 1 3
+chmod 666 /dev/null
+mknod /dev/zero c 1 5
+mknod /dev/random c 1 8
+mknod /dev/urandom c 1 9
+mknod /dev/tty c 5 0
+chmod 666 /dev/tty
 mknod /dev/tty2 c 4 2
 mknod /dev/tty3 c 4 3
 mknod /dev/tty4 c 4 4
+mknod /dev/tty5 c 4 5
+mknod /dev/tty6 c 4 6
+mknod /dev/tty7 c 4 7
+mknod /dev/tty8 c 4 8
+mknod /dev/tty9 c 4 9
+mknod /dev/fb0 c 29 0
